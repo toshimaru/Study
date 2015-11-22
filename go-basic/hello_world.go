ada030a1391d9c5e70d0000a1f51bdd34323234b
toshi <me@toshimaru.net>
2015-11-23 02:22:19 +0900

    Add hello_world.go

---
+++ go-basic/hello_world.go
@@ -0,0 +1,14 @@
+// http://gihyo.jp/dev/feature/01/go_4beginners/0001?page=2
+//
+// $ go run hello_world.go
+// $ go build hello_world.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
