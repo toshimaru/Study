bddfc6d015e578139974071cc6f81c01068447d4
toshimaru <me@toshimaru.net>
2017-03-09 10:19:29 +0900

    0 is false, 1 is true

---
+++ c-basic/num_true_or_false.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+  if (0) {
+    printf("0 is true");
+  } else {
+    printf("0 is false");
+  }
+    
+  printf("\n");
+  
+  if (1) {
+    printf("1 is true");
+  } else {
+    printf("1 is false");
+  }
+}
