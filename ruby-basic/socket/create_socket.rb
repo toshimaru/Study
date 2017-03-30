d708757dcd276b5d9fc4b2893bcad489181fce13
Toshimaru <me@toshimaru.net>
2017-03-31 01:37:12 +0900

    Add create_socket

---
+++ ruby-basic/socket/create_socket.rb
@@ -0,0 +1,9 @@
+require "socket"
+
+p Socket::AF_INET
+p Socket::SOCK_STREAM
+# socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+
+p :INET6
+p :STREAM
+socket = Socket.new(:INET6, :STREAM)
