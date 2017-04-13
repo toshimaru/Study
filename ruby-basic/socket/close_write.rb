a76b31076516335e7e0c5c8e2e5629593b5988a2
toshimaru <me@toshimaru.net>
2017-04-13 15:30:01 +0900

    connection close_write/close_read

---
+++ ruby-basic/socket/close_write.rb
@@ -0,0 +1,10 @@
+require 'socket'
+
+server = Socket.new(:INET, :STREAM)
+addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+server.bind(addr)
+server.listen(128)
+
+connection, _ = server.accept
+connection.close_write # close write stream
+connection.close_read # close write/read
