3556dba6bdad226451e1f805c1e546bdaf15403f
toshimaru <me@toshimaru.net>
2017-04-13 15:21:58 +0900

    Add comment

--- ruby-basic/socket/naive_accept_loop.rb
+++ ruby-basic/socket/naive_accept_loop.rb
@@ -5,6 +5,9 @@ addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
 server.bind(addr)
 server.listen(128)
 
+# netstat -ant | grep LISTEN
+# => `*.4481(LISTEN)` will appear
+
 loop do
   connection, _ = server.accept
   connection.close
