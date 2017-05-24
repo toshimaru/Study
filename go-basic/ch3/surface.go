1e986f828d00b1011dc430e664e04b146286e007
toshimaru <me@toshimaru.net>
2017-05-24 09:48:50 +0900

    Oops

--- go-basic/ch3/surface.go
+++ go-basic/ch3/surface.go
@@ -18,7 +18,7 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"style='stroke: gray; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 
 	for i := 0; i < cells; i++ {
@@ -27,7 +27,7 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+i, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' />\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
