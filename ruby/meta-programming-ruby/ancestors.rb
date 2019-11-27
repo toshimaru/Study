12a37643ce81b968f8e8e940014f43934e220845
toshimaru <me@toshimaru.net>
2019-11-27 10:39:14 +0900

    Update ancestors.rb

--- ruby/meta-programming-ruby/ancestors.rb
+++ ruby/meta-programming-ruby/ancestors.rb
@@ -1,15 +1,19 @@
-module MyModule
+module MyModule1
   def my_method
     puts "my_method"
   end
 end
 
+module MyModule2
+end
+
 class C
-  include MyModule
+  include MyModule1
+  include MyModule2
 end
 
 class D < C
 end
 
 p D.ancestors
-# => [D, C, MyModule, Object, Kernel, BasicObject]
+# => [D, C, MyModule2, MyModule1, Object, Kernel, BasicObject]
