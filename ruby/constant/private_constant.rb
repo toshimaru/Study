025c832378c44f1ffbaad8a48edd7b0c59d87504
toshimaru <me@toshimaru.net>
2020-02-19 09:53:13 +0900

    Add constant for singleton class

--- ruby/constant/private_constant.rb
+++ ruby/constant/private_constant.rb
@@ -7,6 +7,10 @@ module A
 
   class << self
     D = "private?"
+
+    def const_test
+      puts D
+    end
   end
 
   E = "private?"
@@ -17,3 +21,9 @@ puts A::B
 puts A::C
 # puts A::D # => uninitialized constant A::D (NameError)
 # puts A::E # => private constant A::E referenced (NameError)
+
+# retrieve constant D
+A.const_test
+class << A
+  puts D
+end
