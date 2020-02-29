21d44875212160e0a4d9a61194a636a51e8556c5
toshimaru <me@toshimaru.net>
2020-02-29 11:41:56 +0900

    Update ruby.rb

--- monty_hall_problem/ruby.rb
+++ monty_hall_problem/ruby.rb
@@ -3,17 +3,16 @@
 def monty_hall(resample)
   choices = (0..2).to_a
 
-  1000.times.count do
+  10_000.times.count do
     answer = choices.sample
     select = choices.sample
     wrong_answer = (choices - [select, answer]).sample
     
     select = (choices - [wrong_answer, select]).first if resample
 
-    # puts "answer: #{answer}, select: #{select}, wrong_answer: #{wrong_answer}"
     select == answer
   end
 end
 
-puts monty_hall true
-puts monty_hall false
+puts "resample correct count: #{monty_hall(true)}"
+puts "not resample correct count: #{monty_hall(false)}"
