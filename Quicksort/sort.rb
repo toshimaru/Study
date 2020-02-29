15f7f5613dabb51580f063ecac88cf02c6cd88f3
toshimaru <me@toshimaru.net>
2020-02-29 19:39:36 +0900

    Use `Array.new` to create fixed length array

--- Quicksort/sort.rb
+++ Quicksort/sort.rb
@@ -1,12 +1,9 @@
-
 def qsort(ary)
-    return ary if ary.length.zero?
-    pivot = ary.shift
-    qsort(ary.select{|i| i < pivot}) + [pivot] + qsort(ary.select{|i| i >= pivot})
+  return ary if ary.length.zero?
+  pivot = ary.shift
+  qsort(ary.select{|i| i < pivot}) + [pivot] + qsort(ary.select{|i| i >= pivot})
 end
 
-array = []
-10.times{ array.push(rand(100)) }
-
-p array
-p qsort(array)
+ary = Array.new(10) { rand(100) }
+p ary
+p qsort(ary)
