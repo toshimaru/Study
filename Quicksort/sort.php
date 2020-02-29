f5c0a6abec21452e404b9f5ef23e056cfa347804
toshimaru <me@toshimaru.net>
2020-02-29 20:06:58 +0900

    Refactoring PHP QuickSort

--- Quicksort/sort.php
+++ Quicksort/sort.php
@@ -3,28 +3,25 @@
 function qsort(array $ary) {
   if (empty($ary)) return $ary;
 
-  $lesserAndGreater = function(array $a, $pivot) {
-    $lesser = array();
-    $greater = array();
-    foreach ($a as $value) {
+  $lesserAndGreater = function(array $ary, int $pivot) {
+    $lesser = [];
+    $greater = [];
+    foreach ($ary as $value) {
       if ($value < $pivot)
         $lesser[] = $value; 
       else
         $greater[] = $value;
     }
-    return array($lesser, $greater);
+    return [$lesser, $greater];
   };
-
   $pivot = array_shift($ary);
-  list($lesser, $greater) = $lesserAndGreater($ary, $pivot);
-  return array_merge(qsort($lesser), array($pivot), qsort($greater));
+  [$lesser, $greater] = $lesserAndGreater($ary, $pivot);
+  return array_merge(qsort($lesser), [$pivot], qsort($greater));
 }
 
-
-$a = array();
-foreach (range(1, 10) as $i) {
+$a = [];
+foreach (range(1, 10) as $i)
   $a[] = rand(1, 100);
-}
 
 print_r($a);
 print_r(qsort($a));
