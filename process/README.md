d52cb8ba0b0f965a2b52d48070dd306750bc229b
toshimaru <me@toshimaru.net>
2020-06-08 08:53:31 +0900

    Add trap

---
+++ process/README.md
@@ -0,0 +1,1 @@
+[Working With Unix Processes — Learn the Fundamentals of Unix Programming in Ruby](https://www.jstorimer.com/products/working-with-unix-processes)
---
+++ process/trap.rb
@@ -0,0 +1,21 @@
+child_processes = 3
+dead_processes = 0
+
+child_processes.times do
+  fork do
+    puts "child_processes"
+    sleep 5
+  end
+end
+
+trap(:CHLD) do
+  puts "child_processes trap"
+  puts Process.wait
+  dead_processes += 1
+  exit if dead_processes = child_processes
+end
+
+loop do
+  puts "loop"
+  sleep 1
+end
