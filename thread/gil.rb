d7fdb5320825b39341424322288984d1468cf0b9
toshimaru <me@toshimaru.net>
2020-07-08 08:59:45 +0900

    concurrent v.s parallel

--- thread/gil.rb
+++ thread/gil.rb
@@ -1,5 +1,5 @@
 # 並行
-threads1 = Array.new(3) do
+threads1 = Array.new(5) do
   Thread.new do
     r = rand(10) # Concurrent(since GIL)
     puts "sleep #{r} seconds"
@@ -7,18 +7,14 @@ threads1 = Array.new(3) do
     puts "sleep #{r} seconds ended"
   end
 end
-
 puts threads1.each(&:value)
 
-########
 
 # 並列
 require 'open-uri'
-
-threads2 = Array.new(3) do
+threads2 = Array.new(5) do
   Thread.new do
-    puts open('https://toshimaru.net') # Parallel(since I/O)
+    puts URI.open('https://toshimaru.net/') # Parallel(since I/O)
   end
 end
-
 puts threads2.each(&:value)
