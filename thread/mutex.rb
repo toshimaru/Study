09812e2e27b5a69418ca240b5c08101aeb8698f7
toshimaru <me@toshimaru.net>
2020-07-29 16:16:35 +0900

    Create mutex.rb

---
+++ thread/mutex.rb
@@ -0,0 +1,29 @@
+
+mutex = Mutex.new
+results = []
+
+Thread.new do
+  mutex.synchronize do
+    puts '1 begin'
+    results << 1
+    puts '1 end'
+  end
+end
+
+Thread.new do
+  mutex.synchronize do
+    puts '2 begin'
+    results << 2
+    puts '2 end'
+  end
+end
+
+mutex.synchronize do
+  puts '3 begin'
+  results << 3
+  puts '3 end'
+end
+
+sleep 2
+
+p results
