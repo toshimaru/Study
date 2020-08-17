849713642164ea106ec381a05be6c46d9dd1f929
toshimaru <me@toshimaru.net>
2020-08-17 09:28:20 +0900

    Create celluloid_xkcd_printer.rb

---
+++ thread/celluloid_xkcd_printer.rb
@@ -0,0 +1,23 @@
+require 'celluloid/autostart'
+require 'net/http'
+
+class XKCDFetcher
+  include Celluloid
+
+  def next
+    response = Net::HTTP.get_response('dynamic.xkcd.com', '/random/comic')
+    random_comic_url = response['Location']
+    random_comic_url
+  end
+end
+
+fetcher = XKCDFetcher.new
+# puts fetcher.async.next
+futures = []
+
+10.times do
+  futures << fetcher.future.next
+end
+futures.each do |future|
+  puts "You should check out #{future.value}"
+end
