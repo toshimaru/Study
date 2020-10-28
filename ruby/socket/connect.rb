b9450e3c068d5efdf140c150a27a9b6bcf777e65
toshimaru <me@toshimaru.net>
2020-10-28 16:11:52 +0900

    Add Errno::ETIMEDOUT example

--- ruby/socket/connect.rb
+++ ruby/socket/connect.rb
@@ -2,4 +2,6 @@ require 'socket'
 
 socket = Socket.new(:INET, :STREAM)
 remote_addr = Socket.pack_sockaddr_in(80, 'google.com')
+# remote_addr = Socket.pack_sockaddr_in(70, 'google.com')
+#=> Operation timed out - connect(2) for x.x.x.x (Errno::ETIMEDOUT)
 socket.connect(remote_addr)
