dfc9f55eb95f25bbd3f8d7a3e151fce1e5c207ab
toshimaru <me@toshimaru.net>
2021-01-15 10:03:10 +0900

    Create process_per_connection.rb

---
+++ ruby/socket/ftp/process_per_connection.rb
@@ -0,0 +1,49 @@
+require 'socket'
+require_relative '../command_handler'
+
+module FTP
+  class ProcessPerConnection
+    CRLF = "\r\n"
+    
+    def initialize(port = 21)
+      @control_socket = TCPServer.new(port)
+      trap(:INT) { exit }
+    end
+
+    def gets
+      res = @client.gets(CRLF)
+      res
+    end
+
+    def respond(message) 
+      @client.write(message)
+      @client.write(CRLF)
+    end
+
+    def run
+      loop do
+        @client = @control_socket.accept
+
+        pid = fork do
+          respond "220 OHAI"
+          handler = CommandHandler.new(self)
+          loop do
+            request = gets
+
+            if request
+              respond handler.handle(request)
+            else
+              @client.close
+              break
+            end
+          end
+        end
+
+        Process.detach(pid)
+      end
+    end
+  end
+end
+
+server = FTP::ProcessPerConnection.new(4481)
+server.run
