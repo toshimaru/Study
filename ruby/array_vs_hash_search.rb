c8554cca3335b5c5b65985db3c3d3f548fb4adc7
toshimaru <me@toshimaru.net>
2022-03-16 12:10:23 +0900

    Update array_vs_hash_search.rb

--- ruby/array_vs_hash_search.rb
+++ ruby/array_vs_hash_search.rb
@@ -1,19 +1,19 @@
 ary = Array.new(10_000) { rand(1...100_000_000) }.freeze
-hash = ary.each_with_object({}) {|h, n| n[h] = h }.freeze
+hash = ary.to_h {|n| [n, n] }.freeze
+# p ary.take(10)
+# p hash.take(10).to_h
+RANDOM_NUMBERS = Array.new(10_000) { rand(1...100_000_000)  }.freeze
 
 require 'benchmark'
-
-random_numbers = Array.new(10_000) { rand(1...100_000_000)  }.freeze
-
 Benchmark.bm 10 do |r|
   r.report "Array" do
-    random_numbers.each do |n|
+    RANDOM_NUMBERS.each do |n|
       ary.include? n
     end
   end
   r.report "Hash" do
-    random_numbers.each do |n|
-      hash[n]
+    RANDOM_NUMBERS.each do |n|
+      hash.key? n
     end
   end
 end
