d34ae1ff9c12ef6edb2bc6baa4db8e0c132acbd0
toshimaru <me@toshimaru.net>
2022-05-27 03:42:27 +0900

    Update 7.rb

--- 10-problems/7.rb
+++ 10-problems/7.rb
@@ -5,10 +5,47 @@ def slow_gas_station(gas, cost)
   traversed_i || -1
 end
 
-def can_traverse?(gas, cost, start)
+def my_gas_station(gas, cost)
+  return - 1 if cost.sum > gas.sum
+  i, n = 0, gas.size
+
+  while i <= n 
+    remaining = 0
+    gas.size.times.with_index(1) do |j, idx|
+      j = (i + j) % n
+      remaining += gas[j] - cost[j]
+      if remaining < 0
+        i = j
+        break
+      elsif idx == n
+        return i
+      end
+    end
+    i += 1
+  end
+
+  -1
+end
+
+def gas_station(gas, cost)
+  remaining = prev_remaining = candidate = 0
   n = gas.size
-  remaining = 0
-  i = start
+
+  n.times do |i|
+    remaining += gas[i] - cost[i]
+    if remaining < 0
+      candidate = i + 1
+      prev_remaining += remaining
+      remaining = 0
+    end
+  end
+  
+  return -1 if candidate == n || remaining + prev_remaining < 0
+  candidate
+end
+
+def can_traverse?(gas, cost, start)
+  n, i, remaining = gas.size, start, 0
   started = false
 
   while i != start || !started
@@ -24,7 +61,18 @@ end
 gas  = [1,5,3,3,5,3,1,3,4,5]
 cost = [5,2,2,8,2,4,2,5,1,2]
 p slow_gas_station(gas, cost)
+p my_gas_station(gas, cost)
+p gas_station(gas, cost)
 
 gas  = [2,2,2,2,2,2]
 cost = [1,1,1,1,1,50]
 p slow_gas_station(gas, cost)
+p my_gas_station(gas, cost)
+p gas_station(gas, cost)
+
+
+gas  = [1,1,1,10]
+cost = [2,2,2,2]
+p slow_gas_station(gas, cost)
+p my_gas_station(gas, cost)
+p gas_station(gas, cost)
