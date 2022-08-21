ccc0db47d25b61431fa8caf68ba00e9872601696
toshimaru <me@toshimaru.net>
2022-08-21 15:49:23 +0900

    benchmark

--- fibo/fibo.rb
+++ fibo/fibo.rb
@@ -20,3 +20,20 @@ end
 p fibonacci_array(11)
 puts fibo(10)
 puts fibonacci(10)
+
+require 'benchmark/ips'
+Benchmark.ips do |x|
+  SAMPLE_NUMS = (10..30).to_a.sample(5).freeze
+
+  x.report("fibonacci_array") do
+    SAMPLE_NUMS.each { |n| fibonacci_array(n+1) }
+  end
+  x.report("fibonacci") do
+    SAMPLE_NUMS.each { |n| fibonacci(n) }
+  end
+  x.report("fibo") do
+    SAMPLE_NUMS.each { |n| fibo(n) }
+  end
+
+  x.compare!
+end
