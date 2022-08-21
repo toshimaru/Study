ca1168b215eae55cb3185576b53c5b9e2340d033
toshimaru <me@toshimaru.net>
2022-08-21 15:00:49 +0900

    Fibonacci in Ruby

--- fibo/fibo.rb
+++ fibo/fibo.rb
@@ -1,8 +1,20 @@
-
 def fibo(n)
-  return n if (0..1).include? n
+  return n if n == 0 || n == 1
   fibo(n-2) + fibo(n-1)
 end
 
-p fibo(30)
+def fibo2(n)
+  result, a, b = 0, 0, 1
+  n.times do
+    result = a
+    a, b = b, a + b
+    puts result
+  end
+  puts result
+end
 
+10.times { |i|
+  puts fibo(i)
+}
+
+fibo2(10)
