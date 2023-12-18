2614216e9e5bc97542d255d7af31d62983ef1c34
Toshimaru <me@toshimaru.net>
2023-12-18 14:53:48 +0900

    Update private_constant.rb

--- ruby/constant/private_constant.rb
+++ ruby/constant/private_constant.rb
@@ -1,29 +1,29 @@
 module A
-  B = "private?"
+  B = "public constant: B"
 
   private
 
-  C = "private?"
+  C = "private but public constant: C"
 
   class << self
-    D = "private?"
+    D = "class constant: D"
 
     def const_test
       puts D
     end
   end
 
-  E = "private?"
+  E = "private_constant: E"
   private_constant :E
 end
 
 puts A::B
 puts A::C
 # puts A::D # => uninitialized constant A::D (NameError)
-# puts A::E # => private constant A::E referenced (NameError)
-
-# retrieve constant D
+puts A.singleton_class::D
 A.const_test
 class << A
   puts D
 end
+
+# puts A::E # => private constant A::E referenced (NameError)
